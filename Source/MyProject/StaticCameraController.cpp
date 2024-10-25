d698896a732b41c6627ac452fd67b5b95e12e3ef
Serhii-Nesteruk <yarench228@gmail.com>
2024-10-25 22:11:15 +0300

    init

---
+++ Source/MyProject/StaticCameraController.cpp
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "StaticCameraController.h"
+
+#include "MainCameraActor.h"
+#include "Kismet/GameplayStatics.h"
+
+AStaticCameraController::AStaticCameraController()
+{
+	
+}
+
+void AStaticCameraController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	ACameraActor* cameraActor = Cast<ACameraActor>(UGameplayStatics::GetActorOfClass(GetWorld(), AMainCameraActor::StaticClass()));
+	if (cameraActor)
+	{
+		SetViewTarget(cameraActor);
+	}
+
+	APlayerController* PlayerController = GetWorld()->GetFirstPlayerController();
+	if (PlayerController)
+	{
+		PlayerController->bShowMouseCursor = true;
+		PlayerController->bEnableClickEvents = true; 
+		PlayerController->bEnableMouseOverEvents = true;
+	}
+}
+
+void AStaticCameraController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+}
